de51d83eb8abee4c2281f17dffd8640f322754d2
mollah111 <jalammollah111@gmail.com>
2025-01-23 19:13:45 +0600

    Relationship between Category table and SubCategory table

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Category extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function SubCategory ()
+    {
+        return $this->hasMany(SubCategory::class, 'cat_id', 'id');
+    }
 }
+
+
+
+// Category <=> SubCategory
+
+// Category hasMany SubCategory
+
+// BelongsTo
+// hasMany
