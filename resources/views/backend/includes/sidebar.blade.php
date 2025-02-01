6ddeade32a9a52ef2b93d96c73294d46c06dd7b4
mollah111 <jalammollah111@gmail.com>
2025-02-01 13:29:29 +0600

    SubCategory edit-update-delete done

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -1,7 +1,7 @@
  <!-- Main Sidebar Container -->
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
      <!-- Brand Logo -->
-     <a href="{{url('/admin/dashboard')}}" class="brand-link">
+     <a href="{{ url('/admin/dashboard') }}" class="brand-link">
          <img src="{{ asset('backend/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
              class="brand-image img-circle elevation-3" style="opacity: .8">
          <span class="brand-text font-weight-light">Admin</span>
@@ -16,7 +16,7 @@
                      alt="User Image">
              </div>
              <div class="info">
-                 <a href="{{url('/admin/dashboard')}}" class="d-block">Admin</a>
+                 <a href="{{ url('/admin/dashboard') }}" class="d-block">Admin</a>
              </div>
          </div>
 
@@ -47,13 +47,36 @@
                      </a>
                      <ul class="nav nav-treeview">
                          <li class="nav-item">
-                             <a href="{{url('/admin/category/list')}}" class="nav-link">
+                             <a href="{{ url('/admin/category/list') }}" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>List</p>
                              </a>
                          </li>
                          <li class="nav-item">
-                             <a href="{{url('/admin/category/create')}}" class="nav-link">
+                             <a href="{{ url('/admin/category/create') }}" class="nav-link">
+                                 <i class="far fa-circle nav-icon"></i>
+                                 <p>Add</p>
+                             </a>
+                         </li>
+                     </ul>
+                 </li>
+                 <li class="nav-item menu-open">
+                     <a href="#" class="nav-link active">
+                         <i class="nav-icon fas fa-tachometer-alt"></i>
+                         <p>
+                             Sub-Categories
+                             <i class="right fas fa-angle-left"></i>
+                         </p>
+                     </a>
+                     <ul class="nav nav-treeview">
+                         <li class="nav-item">
+                             <a href="{{ url('/admin/sub-category/list') }}" class="nav-link">
+                                 <i class="far fa-circle nav-icon"></i>
+                                 <p>List</p>
+                             </a>
+                         </li>
+                         <li class="nav-item">
+                             <a href="{{ url('/admin/sub-category/create') }}" class="nav-link">
                                  <i class="far fa-circle nav-icon"></i>
                                  <p>Add</p>
                              </a>
@@ -64,19 +87,19 @@
                     <a href="#" class="nav-link active">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
-                            Sub-Categories
+                            Products
                             <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="{{url('/admin/sub-category/list')}}" class="nav-link">
+                            <a href="{{ url('/admin/product/list') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>List</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{url('/admin/sub-category/create')}}" class="nav-link">
+                            <a href="{{ url('/admin/product/create') }}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Add</p>
                             </a>
