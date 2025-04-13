e315b175c804320fb70e4134c66b75a2160f31c5
mollah111 <jalammollah111@gmail.com>
2025-04-13 13:09:12 +0600

    Checkout is in progress

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <section class="checkout-section">
         <div class="container">
-            <form action="" method="post" class="form-group billing-address-form" enctype="multipart/form-data">
+            <form action="{{ url('/confirm-order') }}" method="post" class="form-group billing-address-form"
+                enctype="multipart/form-data">
+                @csrf
                 <div class="row">
                     <div class="col-lg-8 col-md-6">
                         <div class="checkout-wrapper">
@@ -11,24 +13,22 @@
                                 <h4 class="title">Billing / Shipping Details</h4>
                                 <div class="row">
                                     <div class="col-md-6">
-                                        <input type="text" name="name" class="form-control"
-                                            placeholder="Enter Full Name *" />
+                                        <input type="text" name="c_name" class="form-control" placeholder="Enter Full Name *" required/>
                                     </div>
                                     <div class="col-md-6">
-                                        <input type="text" name="phone" class="form-control" placeholder="Phone *" />
+                                        <input type="text" name="c_phone" class="form-control" placeholder="Phone *" required/>
                                     </div>
                                     <div class="col-md-12">
-                                        <textarea rows="4" name="address" class="form-control" id="address" placeholder="Enter Full Address"></textarea>
+                                        <textarea rows="4" name="address" class="form-control" id="address" placeholder="Enter Full Address" required></textarea>
                                     </div>
                                     <div class="col-md-12 mt-3">
                                         <div style="background: lightgrey;padding: 10px;margin-bottom: 10px;">
-                                            <input type="radio" id="inside_dhaka" name="area" value="80" />
+                                            <input type="radio" id="inside_dhaka" name="area" value="80" onclick="grandTotal()" checked/>
                                             <label for="inside_dhaka"
-                                                style="font-size: 18px;font-weight: 600;color: #000;">Inside Dhaka (80
-                                                Tk.)</label>
+                                                style="font-size: 18px;font-weight: 600;color: #000;">Inside Dhaka (80 Tk.)</label>
                                         </div>
                                         <div style="background: lightgrey;padding: 10px;">
-                                            <input type="radio" id="outside_dhaka" name="area" value="150" />
+                                            <input type="radio" id="outside_dhaka" name="area" value="150" onclick="grandTotal()"/>
                                             <label for="outside_dhaka"
                                                 style="font-size: 18px;font-weight: 600;color: #000;">Outside Dhaka (150
                                                 Tk.)</label>
@@ -40,30 +40,37 @@
                     </div>
                     <div class="col-lg-4 col-md-6">
                         <div class="checkout-items-wrapper">
-                           @foreach ($cartProducts as $cart)
-                           <div class="checkout-item-outer">
-                            <div class="checkout-item-left">
-                                <div class="checkout-item-image">
-                                    <img src="{{asset('backend/images/product/'.$cart->product->image)}}" alt="Image" />
-                                </div>
-                                <div class="checkout-item-info">
-                                    <h6 class="checkout-item-name">
-                                        {{$cart->product->name}}
-                                    </h6>
-                                    <p class="checkout-item-price">
-                                        {{$cart->price}} Tk.
-                                    </p>
-                                    <span class="checkout-item-count">
-                                        {{$cart->qty}} item
-                                    </span>
-                                    <br />
-                                    <span class="checkout-item-count">
-                                        Size: {{$cart->size ?? "N.A"}}
-                                    </span>
-                                    <span class="checkout-item-count">
-                                        Color: {{$cart->color ?? "N.A"}}
-                                    </span>
-                                    {{-- <div class="checkout-product-incre-decre">
+                            <?php
+                                $subTotal = 0;
+                            ?>
+                            @foreach ($cartProducts as $cart)
+                            <?php
+                                $subTotal = $subTotal + $cart->qty*$cart->price
+                            ?>
+                                <div class="checkout-item-outer">
+                                    <div class="checkout-item-left">
+                                        <div class="checkout-item-image">
+                                            <img src="{{ asset('backend/images/product/' . $cart->product->image) }}"
+                                                alt="Image" />
+                                        </div>
+                                        <div class="checkout-item-info">
+                                            <h6 class="checkout-item-name">
+                                                {{ $cart->product->name }}
+                                            </h6>
+                                            <p class="checkout-item-price">
+                                                {{ $cart->price }} Tk.
+                                            </p>
+                                            <span class="checkout-item-count">
+                                                {{ $cart->qty }} item
+                                            </span>
+                                            <br />
+                                            <span class="checkout-item-count">
+                                                Size: {{ $cart->size ?? 'N.A' }}
+                                            </span>
+                                            <span class="checkout-item-count">
+                                                Color: {{ $cart->color ?? 'N.A' }}
+                                            </span>
+                                            {{-- <div class="checkout-product-incre-decre">
                                         <button type="button" title="Decrement" class="qty-decrement-btn">
                                             <i class="fas fa-minus"></i>
                                         </button>
@@ -73,19 +80,19 @@
                                             <i class="fas fa-plus"></i>
                                         </button>
                                     </div> --}}
+                                        </div>
+                                    </div>
+                                    <div class="checkout-item-right">
+                                        <a href="{{ url('cart-delete/' . $cart->id) }}" class="delete-btn">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </a>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="checkout-item-right">
-                                <a href="{{url('cart-delete/'.$cart->id)}}" class="delete-btn">
-                                    <i class="fas fa-trash-alt"></i>
-                                </a>
-                            </div>
-                        </div>
-                           @endforeach
+                            @endforeach
                             <div class="sub-total-wrap">
                                 <div class="sub-total-item">
                                     <strong>Sub Total</strong>
-                                    <strong id="subTotal">৳ 300</strong>
+                                    <strong id="subTotal">৳ {{$subTotal}}</strong>
                                 </div>
                                 <div class="sub-total-item">
                                     <strong>Delivery charge</strong>
@@ -93,7 +100,7 @@
                                 </div>
                                 <div class="sub-total-item grand-total">
                                     <strong>Grand Total</strong>
-                                    <strong id="grandTotal">৳ 380</strong>
+                                    <strong id="grandTotal">৳ {{$subTotal+80}}</strong>
                                 </div>
                             </div>
                             <div class="payment-item-outer">
@@ -123,3 +130,11 @@
         </div>
     </section>
 @endsection
+
+@push('script')
+    <script>
+        function grandTotal (){
+
+        }
+    </script>    
+@endpush
